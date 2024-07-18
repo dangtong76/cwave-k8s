998c489694cecf5deedbcefecd360e5a7c86a0a2
dangtong76 <dangtong@gmail.com>
2024-07-18 17:58:34 +0900

    3rd

--- practice/useful_command.sh
+++ practice/useful_command.sh
@@ -1,5 +1,9 @@
 # get credential of EKS
 aws eks update-kubeconfig --region ap-northeast-2 --name cwave
+# get credential of kind
+kind get kubeconfig --internal --name cwave-cluster > bb.txt
+
+# get kind credential 
 
 # test aws cli
 aws s3 ls
@@ -11,4 +15,15 @@ export AWS_PROFILE=cwave
 
 # kubectl rename context-name
 kubectl config rename-context old-name new-name
+# kubectl config rename-context arn:aws:eks:ap-northeast-2:654654260949:cluster/cwave eks
+# kubectl config rename-context kind-cwave-cluster local
+
+## AWS 디스크 생성
+aws ec2 create-volume --volume-type gp2 --size 80 --availability-zone ap-northeast-2a
+
+## AWS 삭제
+aws ec2 delete-volume --volume-id vol-038a54dff454064f6
+
+# AWS 디스크 조회
+aws ec2 describe-volumes --filters Name=status,Values=available Name=availability-zone,Values=ap-northeast-2a
 
